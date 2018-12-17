2b102c73f7e3cc11a1c2e2d94ec29a74c11a1ec7
Stefan Johnson <sj@scrimpycat.io>
2018-12-18 07:45:14 +1100

    Get access token for name/authority

--- lib/resx_dropbox.ex
+++ lib/resx_dropbox.ex
@@ -1,2 +1,11 @@
 defmodule ResxDropbox do
+    require Callback
+
+    defp get_token(name) do
+        case Application.get_env(:resx_dropbox, :token, %{}) do
+            to_token when Callback.is_callback(to_token) -> Callback.call(to_token, [name])
+            tokens when is_map(tokens) -> Map.fetch(tokens, name)
+            token -> { :ok, token }
+        end
+    end
 end
